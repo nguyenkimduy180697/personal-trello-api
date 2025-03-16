bea720f99eb2fdb86283148cf437816b189fa977
CR_Duy_Nguyen <nguyenkimduy.a15@gmail.com>
2025-03-16 23:17:02 +0700

    (...) update WHITELIST_DOMAINS again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   // 'http://localhost:3001'
   // 'http://localhost:3002'
-  'https://personal-trello-app.vercel.app/'
+  'https://personal-trello-app.vercel.app'
 ]
 
 export const BOARD_TYPES = {
